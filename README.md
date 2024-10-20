d71b728f70e23edb2420b1aba05a69f2811cb044
lldan <safinildan@gmail.com>
2024-10-20 14:56:39 +0300

    new readme

--- README.md
+++ README.md
@@ -1,34 +1,12 @@
-<h1 align="center" ><a href="https://lldan.ru/" target="_blank">lldan</a></h1>
-<div align="center">Frontend Developer<br/>Kazan</div>
+I'm the kind of guy who likes angular
 
-<div align="center"><img src="https://wakatime.com/badge/user/018afba7-2ebc-4282-8545-d0250012991b.svg" alt="Total time coded since Oct 4 2023" /></div>
-
-<!--START_SECTION:waka-->
-**I'm a Night 🦉** 
-
-```text
-🌞 Morning                361 commits         ███░░░░░░░░░░░░░░░░░░░░░░   12.67 % 
-🌆 Daytime                1042 commits        █████████░░░░░░░░░░░░░░░░   36.57 % 
-🌃 Evening                1061 commits        █████████░░░░░░░░░░░░░░░░   37.24 % 
-🌙 Night                  385 commits         ███░░░░░░░░░░░░░░░░░░░░░░   13.51 % 
+<!--LEETCODE-->
+leetcode
 ```
-
-
-📊 **This Week I Spent My Time On** 
-
-```text
-💬 Programming Languages: 
-No Activity Tracked This Week
-
-🔥 Editors: 
-No Activity Tracked This Week
-
-🐱‍💻 Projects: 
-No Activity Tracked This Week
+total     799       █████░░░░░░░░░░░░░░░ 24.02 %
+easy      537       █████████████░░░░░░░ 64.70 %
+medium    215       ███░░░░░░░░░░░░░░░░░ 12.35 %
+hard      47        ██░░░░░░░░░░░░░░░░░░ 06.22 %
 ```
+<!--LEETCODE-->
 
-
- Last Updated on 20/10/2024 01:14:48 UTC
-<!--END_SECTION:waka-->
-
-<div align="center"><img src="https://leetcard.jacoblin.cool/lldan"/></div>
---
+++ index.js
@@ -0,0 +1,83 @@
+import fs from "fs"
+
+const md = "<!--LEETCODE-->"
+
+async function main() {
+  try {
+    const data = await fetch("https://leetcode-stats-api.herokuapp.com/lldan");
+    const json = await data.json();
+
+    if (!json.easySolved) {
+      return
+    }
+
+    const info = {
+      total: {solved: json.totalSolved, all: json.totalQuestions},
+      easy: {solved: json.easySolved, all: json.totalEasy},
+      medium: {solved: json.mediumSolved, all: json.totalMedium},
+      hard: {solved: json.hardSolved, all: json.totalHard}
+    }
+    console.log(info)
+
+
+    const readme = fs.readFileSync("./README.md").toString()
+
+    const spl = readme.split(md)
+
+    let stat = `\nleetcode\n`
+    stat += "\`\`\`\n"
+
+    for (const key in info) {
+      stat += up(key)
+      stat += up(info[key].solved.toString())
+      stat += line(info[key].all, info[key].solved)
+      stat += percentage(info[key].solved, info[key].all) + "\n"
+    }
+
+    stat += "\`\`\`\n"
+
+    fs.writeFileSync('./README.md', [spl[0], stat, spl[2]].join(md))
+
+  } catch (e) {
+    console.log("Error")
+  }
+
+}
+
+function up(str) {
+  let res = str;
+
+  while (res.length < 10) {
+    res += " "
+  }
+
+  return res
+}
+
+function line(all, solved) {
+  let res = ""
+
+  const c = (solved / all) * 100
+  let a = 0
+
+  while (a < c) {
+    a += 5
+    res += "█"
+  }
+
+  while (res.length < 20) {
+    res += "░"
+  }
+
+  return res + " "
+}
+
+export function percentage(up, down) {
+  let value = String(((up / down) * 100).toFixed(2));
+  while (value.length !== 5) {
+    value = "0" + value;
+  }
+  return value + " %";
+}
+
+main().then(() => console.log("Finish"))
